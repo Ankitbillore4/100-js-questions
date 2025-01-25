bcc4acece3a75392339ecbfabfbd819480036284
Ankit Billore <billoreankit66@gmail.com>
2025-01-25 15:48:31 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Q1.
+// const Name = "Ankit billore" 
+// const hobby = "Music"
+// console.log("my name is ",Name ,"and hobby is", hobby);
+
+// Q2.
+// const a = 45;
+// const b = 2;
+// const c = 10;
+
+// console.log("The result is : " , a*b-c);
